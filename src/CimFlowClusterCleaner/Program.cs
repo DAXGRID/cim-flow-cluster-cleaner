36c401a7af94a6b1338b5be96f6d8772bbd10613
runeanielsen <runenielsen@runbox.com>
2025-02-24 09:20:38 +0100

    now logs out in case of exception

--- src/CimFlowClusterCleaner/Program.cs
+++ src/CimFlowClusterCleaner/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace CimFlowClusterCleaner;
+﻿using Microsoft.Extensions.Logging;
+
+namespace CimFlowClusterCleaner;
 
 internal static class Program
 {
     public static void Main()
     {
-        Clean.Execute(LoggerFactory.Create(nameof(Clean)));
+        var logger = LoggerFactory.Create(nameof(Program));
+
+        try
+        {
+            Clean.Execute(LoggerFactory.Create(nameof(Clean)));
+        }
+        catch (Exception ex)
+        {
+            logger.LogCritical("{Exception}", ex);
+            throw;
+        }
     }
 }
